0d976a36e1153b943b503b7add10cd80dc309b8b
Joseph Ji <jji@lcs.on.ca>
2025-01-16 14:46:34 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Xinchen Ji on 2025-01-16.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
