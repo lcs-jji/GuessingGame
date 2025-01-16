f5b50095ca5fa13aadd9491f5497202448875f5a
Joseph Ji <jji@lcs.on.ca>
2025-01-16 14:57:16 -0500

    Re-organized project and created a GameView file.

---
+++ GuessingGame/GameView.swift
@@ -0,0 +1,18 @@
+//
+//  Untitled.swift
+//  GuessingGame
+//
+//  Created by Xinchen Ji on 2025-01-16.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#Preview {
+    GameView()
+}
